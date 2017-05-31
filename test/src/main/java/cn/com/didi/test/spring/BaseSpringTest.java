b7e1b85b60b628a42a1012e45eaf9532a436e2ed
xielm <xieleimin@vip.qq.com>
2017-05-31 13:59:04 +0800

    修改注释

--- test/src/main/java/cn/com/didi/test/spring/BaseSpringTest.java
+++ test/src/main/java/cn/com/didi/test/spring/BaseSpringTest.java
@@ -5,20 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.TestExecutionListeners.MergeMode;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import cn.com.didi.test.frame.AnnotionBuilder;
 import cn.com.didi.test.spring.annotion.Builder;
 import cn.com.didi.test.spring.listener.SpringLog4jListener;
 
-/**
- * <p>标题: 单元测试基类</p>
- * <p>描述: 单元测试基类 </p>
- * <p>版权: 税友软件集团股份有限公司</p>
- * <p>创建时间:2015-10-8 </p>
- * <p>作者：谢磊敏</p>
- * <p>修改历史记录：</p>
- */
 //@RunWith(SpringJUnit4ClassRunner.class)
 @RunWith(MySpringJuniT4Runner.class)
 @Builder(value=AnnotionBuilder.class)
--- thirdExt/src/main/java/cn/com/didi/thirdExt/http/IHttpService.java
+++ thirdExt/src/main/java/cn/com/didi/thirdExt/http/IHttpService.java
@@ -1,13 +1,6 @@
 package cn.com.didi.thirdExt.http;
 
-/**
- * 标题: IHttpService.java<br>
- * 描述: <br>
- * 版权: 税友软件集团股份有限公司 <br>
- * 创建时间: 2016年10月12日<br>
- * 作者: xielm <br>
- * 修改历史记录：<br>
-*/
+
 public interface IHttpService {
 	/**
 	 * @param handler
